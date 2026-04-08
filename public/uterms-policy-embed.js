b29235d255fa6f0ea0a046e80b068619bd4a43bb
eddielok <okoklok@gmail.com>
2026-04-08 08:51:38 +0100

    add embed to API

--- public/uterms-policy-embed.js
+++ public/uterms-policy-embed.js
@@ -34,7 +34,7 @@
     container.innerHTML =
       '<p style="font-family:sans-serif;color:#9ca3af;text-align:center;padding:2rem 1rem;">Loading privacy policy\u2026</p>';
 
-    fetch(API_BASE + "/api/policy/" + encodeURIComponent(userId))
+    fetch(API_BASE + "/api/embed/policy/" + encodeURIComponent(userId))
       .then(function (res) {
         if (!res.ok) throw new Error("HTTP " + res.status);
         return res.json();
